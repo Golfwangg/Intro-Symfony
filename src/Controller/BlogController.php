edde748321cd132f6b6509e53867b543e56cb235
William Tassy <william.tassy11@gmail.com>
2019-05-14 19:09:51 +0200

    Quest 04 : Routing avancé

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,15 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/blog", name="blog_")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/index", name="index")
      */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Golfwang',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug<^[a-z0-9-]+$>?Article Sans Titre}", name="show")
+     *
+     */
+    public function show($slug)
+    {
+        $slug = str_replace('-', ' ', $slug);
+        $slug = ucwords($slug);
+        return $this->render('blog/show.html.twig', [
+            "slug" => $slug
         ]);
     }
 }
