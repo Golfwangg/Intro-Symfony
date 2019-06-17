1808723090a6c51bbb52378f6a3e755ddf92fb67
William Tassy <william.tassy11@gmail.com>
2019-06-17 10:40:13 +0200

    Quest 13 - EntityType

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -2,10 +2,12 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Article;
+use App\Entity\Tag;
 
 class ArticleType extends AbstractType
 {
@@ -15,7 +17,13 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false,
+                'choice_label' => 'name',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
